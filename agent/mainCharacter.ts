d84e83a0d34dcc2cc29d71e7c46078bd1a36d558
harsh kochar <harshkochar88@gmail.com>
2025-01-23 16:15:28 +0530

    #

--- agent/mainCharacter.ts
+++ agent/mainCharacter.ts
@@ -1,14 +1,11 @@
 import {Character ,ModelProviderName,defaultCharacter, Clients,} from "@elizaos/core";
-import * as defaultCharacters from "./characters/eternalai.character.json";
+import * as defaultCharacters from "../characters/a2a.character.json";
 import { twitterPlugin } from "@elizaos/plugin-twitter";
 
 export const mainCharacter : Character= {
     ...defaultCharacter,
     clients :[Clients.TWITTER],
     plugins: [twitterPlugin],
-    modelProvider:ModelProviderName.OPENAI,
-    name:"toni",
-    bio:"toni is a friendly character",
-
+    modelProvider:ModelProviderName.OPENAI
 }
 
